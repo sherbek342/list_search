600f6f29f983256d873bc04fb3a80551dd29d51b
sherbek6 <sherbek18rustamov@gmail.com>
2023-07-05 09:34:18 +0300

    dunee

--- find11_min_odd.py
+++ find11_min_odd.py
@@ -5,12 +5,20 @@ def find_min_odd(data):
         data: list of numbers
     returns: minimum odd number in the list
     """
+    
+    list = []
+    k = 0
+    while k < len(data):
+        if data[k]%2 != 0:
+            list.append(data[k])
+        k +=1
+
     i = 0 
-    mn = data[0]
-    while i < len(data):
-        if mn > data[i]:
-            if data[i]%2!=0:
-                mn = data[i]
+    mn = list[0]
+    while i < len(list):
+        if mn > list[i]:
+            if list[i]%2!=0:
+                mn = list[i]
         i += 1
     return mn
-print(find_min_odd(data=[1,-2,4,5,6,4,7]))
+print(find_min_odd(data=[22,3,4,5,6,4,7]))
